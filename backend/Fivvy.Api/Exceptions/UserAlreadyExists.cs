9d3d71057efb4d9dcd4f12ba6cc9c6cb77ab08de
Olgu Değirmenci <olgudegirmenci34@gmail.com>
2025-10-03 04:35:55 +0300

    update .gitignore

--- backend/Fivvy.Api/Exceptions/UserAlreadyExists.cs
+++
@@ -1,8 +0,0 @@
-
-namespace Fivvy.Api.Exceptions;
-
-public class UsernameAlreadyExists : Exception
-{
-    public UsernameAlreadyExists() : base("Username already exists") { }
-    public UsernameAlreadyExists(string message, Exception innerException) : base(message, innerException) { }
-}
